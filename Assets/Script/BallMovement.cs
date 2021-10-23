7cf14928b13ba94b7c4518d821c2671778400b79
FauzanNr <fauzannur.fnr@gmail.com>
2021-10-23 08:48:36 +0800

    add problem3 solution

--- Assets/Script/BallMovement.cs
+++ Assets/Script/BallMovement.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallMovement: MonoBehaviour {
-	// Start is called before the first frame update
+	public float speed = 100;
+	private Rigidbody rg;
 	void Start() {
+		rg = GetComponent<Rigidbody>();
+		InvokeRepeating( "move", 2, 5 );
 
 	}
 
-	// Update is called once per frame
-	void FixedUpdate() {
-		transform.position += transform.forward * Time.deltaTime * 2;
+	void move() {
+		rg.velocity = transform.forward * Time.deltaTime * speed;
 	}
 }
