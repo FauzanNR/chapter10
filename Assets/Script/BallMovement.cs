07c067fe417e681bf55142b35955c7052f9b2325
FauzanNr <fauzannur.fnr@gmail.com>
2021-10-30 15:57:40 +0800

    added problem 6 solution

--- Assets/Script/BallMovement.cs
+++ Assets/Script/BallMovement.cs
@@ -2,24 +2,20 @@
 
 public class BallMovement: MonoBehaviour {
 
-	public LayerMask layerMask;
-	public Camera camera;
 	public float speed = 100;
 	private Rigidbody rg;
-
 	void Awake() {
 		rg = GetComponent<Rigidbody>();
 	}
 
 	void FixedUpdate() {
-		Ray ray = camera.ScreenPointToRay( Input.mousePosition );
-		if(Physics.Raycast( ray, out RaycastHit rayHit, float.MaxValue, layerMask ) && Input.GetMouseButton( 0 )) {
-			move( rayHit.point );
-		}
+		var h = Input.GetAxisRaw( "Horizontal" );
+		var v = Input.GetAxisRaw( "Vertical" );
+		move( h, v );
 	}
 
-	void move(Vector3 position) {
-		position = position - transform.position;
+	void move(float horizontal, float vertical) {
+		var position = new Vector3( horizontal, 0f, vertical );
 		position = position.normalized * speed;
 		rg.AddForce( position );
 	}
