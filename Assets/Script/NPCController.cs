93b1954c001afc3602b503f98070de22d8afbae1
FauzanNr <fauzannur.fnr@gmail.com>
2021-10-30 16:51:27 +0800

    added problem7 solution

---
+++ Assets/Script/NPCController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController: MonoBehaviour {
+	private GameObject player;
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag( "Player" );
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject == player && other.isTrigger == false) {
+			ScoreManager.instance.addScore();
+			this.gameObject.SetActive( false );
+		}
+	}
+}
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager: MonoBehaviour {
+
+	public Text textScore;
+	public int score {
+		get;
+		private set;
+	}
+	public static ScoreManager instance {
+		get;
+		private set;
+	}
+
+	void Awake() {
+		instance = this;
+	}
+
+	public void addScore() {
+		score += 1;
+	}
+
+	void Update() {
+		textScore.text = score.ToString();
+	}
+
+}
