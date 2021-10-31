d610461a806d2956294dd167231b0618f2f7014a
FauzanNr <fauzannur.fnr@gmail.com>
2021-10-31 21:18:47 +0800

    added problem8 solution

--- Assets/Script/NPCController.cs
+++ Assets/Script/NPCController.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCController: MonoBehaviour {
+
+	float time = 0f;
 	private GameObject player;
 
 	void Awake() {
 		player = GameObject.FindGameObjectWithTag( "Player" );
+	}
+
+	void OnDisable() {
+		Invoke( "activate", 3 );
+	}
+
+	void activate() {
+		transform.position = ObjectPool.instance.generateRandomPosition();
+		this.gameObject.SetActive( true );
 	}
 
 	void OnTriggerEnter(Collider other) {
--- Assets/Script/ObjectPool.cs
+++ Assets/Script/ObjectPool.cs
@@ -3,38 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool: MonoBehaviour {
-	//ObjectPool instance;
+
+
+	public LayerMask layer;
 	public int maxNPCNumber = 5;
 	GameObject ground;
 	[SerializeField] private GameObject npcPrefab;
 	private List<GameObject> npcPool = new List<GameObject>();
-	//Transform position;
 	float scale = 1f;
-
-
+	float time = 0.0f;
+	public static ObjectPool instance {
+		get;
+		private set;
+	}
+	void Awake() {
+		instance = this;
+	}
 	void Start() {
 		ground = GameObject.FindGameObjectWithTag( "Ground" );
-		float moveAreaX = ground.GetComponent<Renderer>().bounds.size.x / 2;
-		float moveAreaZ = ground.GetComponent<Renderer>().bounds.size.z / 2;
-		Vector3 center = ground.GetComponent<Renderer>().bounds.center;
 
 		var randomNumber = Random.Range( 1, maxNPCNumber );
 		for(int i = 0; i < randomNumber; i++) {
-
-			var targetCoordsX = center.x + Random.Range( -moveAreaX * scale, moveAreaX * scale );
-			var targetCoordsZ = center.z + Random.Range( -moveAreaZ * scale, moveAreaZ * scale );
-
-			//Debug.Log( new Vector3( targetCoordsX, 0.5f, targetCoordsZ ) );
-			var position = new Vector3( targetCoordsX, 0.5f, targetCoordsZ );
-
-			var obj = Instantiate( npcPrefab, position, Quaternion.identity );
-			//obj.SetActive( false );
+			var obj = Instantiate( npcPrefab, generateRandomPosition(), Quaternion.identity );
 			npcPool.Add( obj );
 		}
 	}
 
-	// Update is called once per frame
 	void Update() {
-
+		//var npcs = respawnNpc();
+		//Debug.Log( npcs );
+		//if(npcs != null) {
+		//	time += Time.deltaTime;
+		//	int seconds = ( int )(time % 60);
+		//	Debug.Log( seconds );
+		//	if(seconds == 3) {
+		//		npcs.transform.position = generateRandomPosition();
+		//		npcs.SetActive( true );
+		//		seconds = 0;
+		//		time = 0.0f;
+		//	}
+		//}
 	}
+
+	public Vector3 generateRandomPosition() {
+
+		float moveAreaX = ground.GetComponent<Renderer>().bounds.size.x / 2;
+		float moveAreaZ = ground.GetComponent<Renderer>().bounds.size.z / 2;
+		Vector3 center = ground.GetComponent<Renderer>().bounds.center;
+
+		var targetCoordsX = center.x + Random.Range( -moveAreaX * scale, moveAreaX * scale );
+		var targetCoordsZ = center.z + Random.Range( -moveAreaZ * scale, moveAreaZ * scale );
+		var position = new Vector3( targetCoordsX, 0.5f, targetCoordsZ );
+
+		while(Physics.CheckSphere( position, 1, layer )) {
+			targetCoordsX = center.x + Random.Range( -moveAreaX * scale, moveAreaX * scale );
+			targetCoordsZ = center.z + Random.Range( -moveAreaZ * scale, moveAreaZ * scale );
+			position = new Vector3( targetCoordsX, 0.5f, targetCoordsZ );
+			Debug.Log( "collide" );
+		}
+
+		return position;
+	}
+
+	//GameObject respawnNpc() {
+	//	foreach(var npc in npcPool) {
+	//		if(!npc.activeInHierarchy) {
+	//			return npc;
+	//		}
+	//	}
+	//	return null;
+	//}
 }
